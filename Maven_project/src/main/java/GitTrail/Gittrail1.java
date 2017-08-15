6d3a169743c7a9178a382d277935fc34de939bfd
lenovo <lenovo@DESKTOP-DRIB81Q>
2017-08-15 22:57:48 +0530

    added trail

---
+++ Maven_project/src/main/java/GitTrail/Gittrail1.java
@@ -0,0 +1,11 @@
+package GitTrail;
+
+public class Gittrail1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi welcome in git");
+	}
+
+}

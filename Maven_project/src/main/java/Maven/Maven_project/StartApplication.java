d36f272f52c9ec675d52f29f6f3f033a56e7bf52
lenovo <lenovo@DESKTOP-DRIB81Q>
2017-08-15 22:46:53 +0530

    my first commmit

---
+++ Maven_project/src/main/java/Maven/Maven_project/StartApplication.java
@@ -0,0 +1,121 @@
+package Maven.Maven_project;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import java.io.File;
+ 
+import java.net.MalformedURLException;
+ 
+import java.net.URL;
+ 
+import java.util.concurrent.TimeUnit;
+ 
+ 
+ 
+import org.openqa.selenium.By;
+ 
+import org.openqa.selenium.remote.CapabilityType;
+ 
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+ 
+ 
+ @Test
+public class StartApplication {
+ 
+       
+ 
+private static AndroidDriver driver;
+ 
+public static void main(String[] args) throws MalformedURLException, InterruptedException 
+{
+ 
+ 
+ // Create object of DesiredCapabilities class                             
+ 
+DesiredCapabilities capabilities = new DesiredCapabilities();
+ 
+ 
+ 
+// Optional
+ 
+capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+ 
+ 
+// Specify the device name (any name)
+ 
+capabilities.setCapability("deviceName", "My New Phone");
+ 
+ 
+// Platform version
+ 
+capabilities.setCapability("platformVersion", "4.4.2");
+ 
+ 
+// platform name
+ 
+capabilities.setCapability("platformName", "Android");
+ 
+ 
+// specify the application package that we copied from appium                
+ 
+//capabilities.setCapability("appPackage", "io.selendroid.testapp");
+//capabilities.setCapability("appPackage", "com.zipgo.customer");   //ZipGo Customer App will Open
+capabilities.setCapability("appPackage", "com.whatsapp");
+
+ 
+ 
+// specify the application activity that we copied from appium                   
+ 
+//capabilities.setCapability("appActivity", ".HomeScreenActivity");
+
+//capabilities.setCapability("appActivity", "com.zipgo.customer.activities.TransactionActivity");
+capabilities.setCapability("appActivity", ".com.whatsapp.Main");
+
+ 
+// Start android driver I used 4727 port by default it will be 4723
+ 
+driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+ 
+ 
+ 
+// Specify the implicit wait of 5 second
+ 
+driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+ 
+ 
+ 
+// Enter the text in textbox
+ 
+driver.findElement(By.xpath("//android.widget.EditText[@content-desc='my_text_fieldCD']")).sendKeys("Prashant Selenium Appium");
+System.out.println("writing Text : PASS");
+//driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='Login with Google+']")).sendKeys("Prashant Selenium Appium");
+
+ 
+// click on registration button  
+                                   
+driver.findElement(By.id("io.selendroid.testapp:id/startUserRegistration")).click();
+
+System.out.println("click on registration button : PASS");
+ 
+ 
+ 
+// Wait for 10 second
+System.out.println("Please wait for 10 second");
+Thread.sleep(10000);
+
+ 
+ 
+ 
+// close the application
+ driver.quit();
+ 
+ System.out.println("Done");
+ 
+ 
+ 
+                }
+ 
+ 
+ 
+}
---
+++ Maven_project/src/main/java/Maven/Maven_project/TestNGDemo.java
@@ -0,0 +1,56 @@
+package Maven.Maven_project;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@Test
+	public void TestNG1() throws InterruptedException {
+		System.out.println("Hi Welcome in TestNG world");
+		
+		System.out.println("Hi Welcome to ZipGo Dashboard");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://beta.zipgo.in/sign_in"); //Only local connections are allowed(no www)
+		//driver.get("https://www.zipgo.in/");
+		//driver.findElement(By.className("auth0-lock-social-button-text")).click();
+		driver.findElement(By.xpath("//*[@id='auth0-lock-container-1']/div/div[2]/div/div/div/div[3]/span/div/div/div/div/div/div/div[1]/div[1]/div/div/div[1]/button")).click();
+	//	driver.findElement(By.id("//*[@id='view_container']/form/div[2]/div/div[1]/div[1]/div/div[1]")).sendKeys("prashant.ranjan@zipgo.in");
+	//	driver.findElement(By.id("//*[@id='identifierNext']/content/span")).clear();
+	//	Thread.sleep(5000);
+		driver.findElement(By.xpath("//div[@role='presentation']//descendant::input[@id='identifierId']")).sendKeys("prashant.ranjan@zipgo.in");
+		driver.findElement(By.xpath("//div[@role='presentation']//descendant::span[@class='RveJvd snByac']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//div[@role='presentation']//descendant::input[@type='password']")).sendKeys("Zipgoaug@123");
+		driver.findElement(By.xpath("//div[@role='presentation']//descendant::span[@class='RveJvd snByac']")).click();
+	//	Thread.sleep(7000);
+		WebElement findmap=driver.findElement(By.xpath("//ul[@class='main-navigation-menu']//descendant::i[@class='clip-map']"));
+		WebDriverWait wait=new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOf((findmap)));
+		//hover 
+		Actions bulder = new Actions(driver);
+		bulder.moveToElement(findmap).perform();
+		/*List<WebElement> findmapList=driver.findElements(By.xpath("//li[@class='open']//descendant::ul[@class='dropdown-menu']/li/a/span[@class='title']"));
+		//for(WebElement _object:findmapList) {
+			//System.out.println(_object.getText());
+			
+		}*/
+		System.out.println("=======================Autimation Done=====================================");
+	driver.manage().window().maximize();
+	Thread.sleep(2000);
+	driver.quit();
+
+	}
+		
+	}
+
